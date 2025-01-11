9f92bd333a9c4aac8e07b50102b658b8076d7481
Cleyton-1995 <cleytoncosta.developer@gmail.com>
2025-01-11 11:38:16 -0300

    feat: add Header component

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,29 @@
-import { Text, View } from "react-native";
+import { ScrollView, StatusBar, Text, View } from "react-native";
+import React from "react";
+import { Header } from "../components/Header";
+
+import { getStatusBarHeight } from "react-native-iphone-x-helper";
 
 export default function Index() {
   return (
-    <View className="w-full h-full flex justify-center items-center">
-      <Text className="text-xl">Tab One</Text>
-    </View>
+    <>
+      <StatusBar
+        barStyle="dark-content"
+        backgroundColor="#E2E8F0"
+        translucent
+      />
+      <ScrollView
+        className="bg-slate-200 px-0.5"
+        style={{ flex: 1 }}
+        showsVerticalScrollIndicator={false}
+      >
+        <View
+          className="w-full px-4"
+          style={{ marginTop: getStatusBarHeight() + 17 }}
+        >
+          <Header />
+        </View>
+      </ScrollView>
+    </>
   );
 }
